ab6eea3abb0a10c8d47cc0f81683de436d95435e
Ngoc Han <ngochan@trycz.vn>
2016-03-30 10:08:58 +0700

    change production mailer seting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,8 @@ ActionMailer::Base.smtp_settings = {
    :enable_starttls_auto => true,
    :address        => 'smtp.gmail.com',
    :port           => 587,
-   :domain         => 'lipans.com.vn',
-   :authentication => 'plain',
+   :domain         => 'gmail.com',
+   :authentication => 'login',
    :user_name      => 'kayltp2706@gmail.com',
    :password       => 'kay2706<>'
 }
@@ -71,11 +71,9 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'lipans.com.vn'}
   config.action_mailer.asset_host = 'http://lipans.com.vn'
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
   
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
